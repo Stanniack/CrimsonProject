481dd9fca0267452a2a4a5d86bf1d5769c7a5a11
Mateus Vitor <stanniack@gmail.com>
2024-01-19 10:03:34 -0300

    Update ExtractOre.java

--- src/main/java/com/mycompany/crimsonproject/scripts/ExtractOre.java
+++ src/main/java/com/mycompany/crimsonproject/scripts/ExtractOre.java
@@ -25,27 +25,81 @@ public class ExtractOre {
         int amountRect = 0;
 
         do {
-            
+
             SegmentedRegions3 sr3 = new SegmentedRegions3();
-
             boolean flagNoDragScreen = false;
+            boolean foundRect = false;
             new TakeScreenShot2().take();
-            
-            //Rectangle condensedResult = sr3
 
-            Rectangle rectResult = new SegmentedRegions2()
-                    .getSegmentedRegion_2WxH_2Xx2Y(Rect1920x1080.CONDENSED_WIDTH1, Rect1920x1080.CONDENSED_WIDTH2, Rect1920x1080.CONDENSED_HEIGHT1,
-                            Rect1920x1080.OVERVIEWMINING_X, Rect1920x1080.OVERVIEWMINING_X2_W_BLOCKSCREEN,
-                            Rect1920x1080.OVERVIEWMINING_Y, Rect1920x1080.OVERVIEWMINING_Y2_H_BLOCKSCREEN);
+            switch (amountRect) {
+                case 0 -> {
+                    /* P1 */
+                    Rectangle condensedRectResult = sr3
+                            .getSegmentedRegion_2WxH_BLOCKSCREEN(Rect1920x1080.CONDENSED_WIDTH1, Rect1920x1080.CONDENSED_WIDTH2, Rect1920x1080.CONDENSED_HEIGHT1,
+                                    Rect1920x1080.OVERVIEWMINING_X, Rect1920x1080.OVERVIEWMINING_X2_W_BLOCKSCREEN,
+                                    Rect1920x1080.OVERVIEWMINING_Y, Rect1920x1080.OVERVIEWMINING_Y2_H_BLOCKSCREEN);
 
-            // for percorrendo lista de rects aqui - utilizar lista ordenada
-            if (rectResult != null) {
-                System.out.printf("Rect found (OVERVIEW->MINING SCORDITE) - Width: %d and height: %d\n", rectResult.width, rectResult.height);
-                new ClickScreen().rightClickEvent(rectResult);
-                amountRect++;
-                flagNoDragScreen = true;
-            } else {
-                System.out.println("rect not found");
+                    /* P2 */
+                    Rectangle scorditeRectResult = sr3
+                            .getSegmentedRegion_Wx2H_BLOCKSCREEN(Rect1920x1080.SCORDITE_WIDTH, Rect1920x1080.SCORDITE_HEIGHT1, Rect1920x1080.SCORDITE_HEIGHT2,
+                                    Rect1920x1080.OVERVIEWMINING_X, Rect1920x1080.OVERVIEWMINING_X2_W_BLOCKSCREEN,
+                                    Rect1920x1080.OVERVIEWMINING_Y, Rect1920x1080.OVERVIEWMINING_Y2_H_BLOCKSCREEN);
+
+                    /* P3 */
+                    Rectangle denseRectResult = sr3
+                            .getSegmentedRegion_2WxH_BLOCKSCREEN(Rect1920x1080.DENSE_WIDHT1, Rect1920x1080.DENSE_WIDHT2, Rect1920x1080.DENSE_HEIGHT1,
+                                    Rect1920x1080.OVERVIEWMINING_X, Rect1920x1080.OVERVIEWMINING_X2_W_BLOCKSCREEN,
+                                    Rect1920x1080.OVERVIEWMINING_Y, Rect1920x1080.OVERVIEWMINING_Y2_H_BLOCKSCREEN);
+
+                    /* P4 */
+                    Rectangle concentratedResult = sr3
+                            .getSegmentedRegionConcentrated_3Wx2H_BLOCKSCREEN(Rect1920x1080.CONCENTRATED_WIDTH1, Rect1920x1080.CONCENTRATED_WIDTH2, Rect1920x1080.CONCENTRATED_WIDTH3,
+                                    Rect1920x1080.CONDENSED_HEIGHT1, Rect1920x1080.CONCENTRATED_HEIGHT2,
+                                    Rect1920x1080.OVERVIEWMINING_X, Rect1920x1080.OVERVIEWMINING_X2_W_BLOCKSCREEN,
+                                    Rect1920x1080.OVERVIEWMINING_Y, Rect1920x1080.OVERVIEWMINING_Y2_H_BLOCKSCREEN);
+
+                    /* P5 */
+                    Rectangle veldsparResult = sr3
+                            .getSegmentedRegion_3Wx2H_BLOCKSCREEN(Rect1920x1080.VELDSPAR_WIDTH1, Rect1920x1080.VELDSPAR_WIDTH2, Rect1920x1080.VELDSPAR_WIDTH3,
+                                    Rect1920x1080.VELDSPAR_HEIGHT1, Rect1920x1080.CONCENTRATED_HEIGHT2,
+                                    Rect1920x1080.OVERVIEWMINING_X, Rect1920x1080.OVERVIEWMINING_X2_W_BLOCKSCREEN,
+                                    Rect1920x1080.OVERVIEWMINING_Y, Rect1920x1080.OVERVIEWMINING_Y2_H_BLOCKSCREEN);
+
+                    if (condensedRectResult != null) {
+                        System.out.printf("Rect found (OVERVIEW->MINING CONDENSED SCORDITE) - Width: %d and height: %d\n", condensedRectResult.width, condensedRectResult.height);
+                        new ClickScreen().rightClickEvent(condensedRectResult);
+                        foundRect = true;
+
+                    } else if (scorditeRectResult != null) {
+                        System.out.printf("Rect found (OVERVIEW->MINING - SCORDITE) - Width: %d and height: %d\n", condensedRectResult.width, condensedRectResult.height);
+                        new ClickScreen().rightClickEvent(scorditeRectResult);
+                        foundRect = true;
+
+                    } else if (denseRectResult != null) {
+                        System.out.printf("Rect found (OVERVIEW->MINING - DENSE VELDSPAR) - Width: %d and height: %d\n", condensedRectResult.width, condensedRectResult.height);
+                        new ClickScreen().rightClickEvent(denseRectResult);
+                        foundRect = true;
+
+                    } else if (concentratedResult != null) {
+                        System.out.printf("Rect found (OVERVIEW->MINING - CONCENTRATED VELDSPAR) - Width: %d and height: %d\n", condensedRectResult.width, condensedRectResult.height);
+                        new ClickScreen().rightClickEvent(concentratedResult);
+                        foundRect = true;
+
+                    } else if (veldsparResult != null) {
+                        System.out.printf("Rect found (OVERVIEW->MINING - VELDSPAR) - Width: %d and height: %d\n", condensedRectResult.width, condensedRectResult.height);
+                        new ClickScreen().rightClickEvent(veldsparResult);
+                        foundRect = true;
+
+                    } else {
+                        System.out.println("rect not found");
+                    }
+
+                    if (foundRect) {
+                        amountRect++;
+                        flagNoDragScreen = true;
+                    }
+                    
+                } // end case 0
             }
 
             if (!flagNoDragScreen) {
