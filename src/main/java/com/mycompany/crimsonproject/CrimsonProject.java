5285a4fa23d366995ff25a999663043b1d66cac1
Mateus Vitor <stanniack@gmail.com>
2024-02-06 11:23:14 -0300

    Refatoração
    Classe CrimsonProject não será mais utilizada.

--- src/main/java/com/mycompany/crimsonproject/CrimsonProject.java
+++ src/main/java/com/mycompany/crimsonproject/CrimsonProject.java
@@ -1,39 +1,12 @@
 package com.mycompany.crimsonproject;
 
-import com.mycompany.crimsonproject.scripts.CheckCargoDeposit;
-import com.mycompany.crimsonproject.scripts.ExtractOre;
-import com.mycompany.crimsonproject.scripts.GetDestination;
-import java.awt.AWTException;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import net.sourceforge.tess4j.TesseractException;
-
 /**
  *
  * @author Stanniack
  */
 public class CrimsonProject {
 
-    public static void main(String[] args) throws InterruptedException {
-        Thread.sleep(4000);
-
-        for (int i = 0; i < 10; i++) {
-            try {
-
-                new CheckCargoDeposit().check();
-                Thread.sleep(18000);
-
-                new GetDestination().getDestination(1);
-                Thread.sleep(30000);
-
-                new ExtractOre().extract();
-                Thread.sleep(35000);
-
-            } catch (InterruptedException | IOException | AWTException | TesseractException ex) {
-                Logger.getLogger(CrimsonProject.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+    public static void main(String[] args) {
 
     }
 }
