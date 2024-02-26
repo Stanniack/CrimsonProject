2506bd270386a3ca08337690d31c5f7df756ee1e
Mateus Vitor <stanniack@gmail.com>
2024-02-26 08:30:43 -0300

    Criação da classe abstrata RobotEvent
    Testando classe abstrata como interface para diminuir linha de códigos.

--- src/main/java/com/mycompany/crimsonproject/robot/ClickScreenEvents.java
+++ src/main/java/com/mycompany/crimsonproject/robot/ClickScreenEvents.java
@@ -2,112 +2,84 @@ package com.mycompany.crimsonproject.robot;
 
 import java.awt.AWTException;
 import java.awt.Rectangle;
-import java.awt.Robot;
-import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
 
 /**
  *
- * @author Stanniack
+ * @author DevMachine
  */
-public class ClickScreenEvents {
-    private static final int SLEEP_MS = 150; 
+public class ClickScreenEvents extends RobotEvent {
 
     public void leftClickCenterButton(Rectangle rect) throws AWTException, InterruptedException {
 
-        int leftClick = InputEvent.BUTTON1_DOWN_MASK;
-
-        Robot bot = new Robot();
-
         /* (Xa = Xo + w/2) | (Ya = Yo + h/2) - Search the center of the rectangle */
-        bot.mouseMove(rect.x + rect.width / 2, rect.y + rect.height / 2);
+        this.bot.mouseMove(rect.x + rect.width / 2, rect.y + rect.height / 2);
+
         Thread.sleep(SLEEP_MS);
-        bot.mousePress(leftClick);
+        this.bot.mousePress(this.leftClick);
         Thread.sleep(SLEEP_MS);
-        bot.mouseRelease(leftClick);
+        this.bot.mouseRelease(this.leftClick);
 
     }
 
     public void rightClickCenterButton(Rectangle rect) throws AWTException, InterruptedException {
 
-        int rightClick = InputEvent.BUTTON3_DOWN_MASK;
-
-        Robot bot = new Robot();
-
         /* (Xa = Xo + w/2) | (Ya = Yo + h/2) - Search the center of the rectangle */
-        bot.mouseMove(rect.x + rect.width / 2, rect.y + rect.height / 2);
+        this.bot.mouseMove(rect.x + rect.width / 2, rect.y + rect.height / 2);
         Thread.sleep(SLEEP_MS);
-        bot.mousePress(rightClick);
+        this.bot.mousePress(rightClick);
         Thread.sleep(SLEEP_MS);
-        bot.mouseRelease(rightClick);
+        this.bot.mouseRelease(rightClick);
 
     }
 
     public void doubleClick(Rectangle rect) throws AWTException, InterruptedException {
 
-        int leftClick = InputEvent.BUTTON1_DOWN_MASK;
-
-        Robot bot = new Robot();
-
         /* (Xa = Xo + w/2) | (Ya = Yo + h/2) - Search the center of the rectangle */
-        bot.mouseMove(rect.x + rect.width / 2, rect.y + rect.height / 2);
+        this.bot.mouseMove(rect.x + rect.width / 2, rect.y + rect.height / 2);
 
         Thread.sleep(SLEEP_MS);
-        bot.mousePress(leftClick);
-        bot.mouseRelease(leftClick);
+        this.bot.mousePress(leftClick);
+        this.bot.mouseRelease(leftClick);
 
         Thread.sleep(SLEEP_MS);
-        bot.mousePress(leftClick);
-        bot.mouseRelease(leftClick);
+        this.bot.mousePress(leftClick);
+        this.bot.mouseRelease(leftClick);
 
     }
 
     public void holdCtrlAndLeftClick(Rectangle rect) throws AWTException, InterruptedException {
 
-        int ctrl = KeyEvent.VK_CONTROL;
-        int leftClick = InputEvent.BUTTON1_DOWN_MASK;
-
-        Robot bot = new Robot();
-
         /* (Xa = Xo + w/2) | (Ya = Yo + h/2) - Search the center of the rectangle */
-        bot.mouseMove(rect.x + rect.width / 2, rect.y + rect.height / 2);
+        this.bot.mouseMove(rect.x + rect.width / 2, rect.y + rect.height / 2);
 
         Thread.sleep(SLEEP_MS);
-        bot.keyPress(ctrl);
+        this.bot.keyPress(this.ctrl);
 
         Thread.sleep(SLEEP_MS);
-        bot.mousePress(leftClick);
+        this.bot.mousePress(this.leftClick);
 
-        bot.keyRelease(ctrl);
-        bot.mouseRelease(leftClick);
+        this.bot.keyRelease(this.ctrl);
+        this.bot.mouseRelease(this.leftClick);
 
     }
 
     /* Actioned when the I.A doesn't found the case and needs to be returned. */
     public void returnCaseLeftClick() throws InterruptedException, AWTException {
 
-        int leftClick = InputEvent.BUTTON1_DOWN_MASK;
-
-        Robot bot = new Robot();
-
-        bot.mouseMove(1230, 720);
+        this.bot.mouseMove(1230, 720);
         Thread.sleep(SLEEP_MS);
-        bot.mousePress(leftClick);
+        this.bot.mousePress(this.leftClick);
         Thread.sleep(SLEEP_MS);
-        bot.mouseRelease(leftClick);
+        this.bot.mouseRelease(this.leftClick);
     }
 
     public void leftClick(int x, int y) throws InterruptedException, AWTException {
 
-        int leftClick = InputEvent.BUTTON1_DOWN_MASK;
-
-        Robot bot = new Robot();
-
-        bot.mouseMove(x, y);
+        this.bot.mouseMove(x, y);
         Thread.sleep(SLEEP_MS);
-        bot.mousePress(leftClick);
+        this.bot.mousePress(this.leftClick);
         Thread.sleep(SLEEP_MS);
-        bot.mouseRelease(leftClick);
+        this.bot.mouseRelease(this.leftClick);
 
     }
 }
---
+++ src/main/java/com/mycompany/crimsonproject/robot/RobotEvent.java
@@ -0,0 +1,33 @@
+package com.mycompany.crimsonproject.robot;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Devmachine
+ */
+abstract class RobotEvent {
+    protected static final int SLEEP_MS = 150; 
+    protected static final int SLEEP_MS2 = 200;
+    
+    protected int leftClick = InputEvent.BUTTON1_DOWN_MASK;
+    protected int rightClick = InputEvent.BUTTON3_DOWN_MASK;
+    protected int ctrl = KeyEvent.VK_CONTROL;
+    
+    protected Robot bot = null;
+    
+    RobotEvent () {
+        try {
+            bot = new Robot();
+        } catch (AWTException ex) {
+            Logger.getLogger(RobotEvent.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+}
