0ec890efe3cbf6fdad4942d2caf4c734c8d61af7
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-06-13 18:46:30 -0300

    Alterando variáveis de tempo para retriever
    A nave retriever requer 1904 segundos para ser totalmente carregada.

--- src/main/java/com/mycompany/crimsonproject/scripts/ExtractOre.java
+++ src/main/java/com/mycompany/crimsonproject/scripts/ExtractOre.java
@@ -37,8 +37,8 @@ public class ExtractOre implements VerifyRectangle {
 
     private static final int LOCKTARGET_MS = 60000;
     private static final int SWITCHFLAG = 7;
-    private static final int TIMETOWAIT_TOBEFILLED_MS = 800000;
-    private static final int TIMETOWAIT_CANNON_MS = 780000;
+    private static final int TIMETOWAIT_TOBEFILLED_MS = 1910000;
+    private static final int TIMETOWAIT_CANNON_MS = 1904000;
     private static final int GOTO_HOMESTATION = 0;
     private static final int CANNON_SLEEP = 2000;
 
