e77c08c3efa27ad1c48a6d5bb1ab4b07802a397f
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-08-17 19:41:08 -0300

    Update Main

--- src/main/java/com/mycompany/crimsonproject/CrimsonProject.java
+++ src/main/java/com/mycompany/crimsonproject/CrimsonProject.java
@@ -1,6 +1,7 @@
 package com.mycompany.crimsonproject;
 
 import com.mycompany.crimsonproject.findpixels.FindPixels;
+import java.io.File;
 import java.io.IOException;
 import org.javatuples.Triplet;
 
@@ -13,13 +14,14 @@ public class CrimsonProject {
     public static void main(String[] args) {
         try {
             double start = System.currentTimeMillis();
-            int area = new FindPixels().pixelContainsColorByRange(1044, 910, 42, 40, new Triplet<>(90,77,52), new Triplet<>(90,77,52));
-            System.out.println("Area cannon 1: " + area);
+            File imageFile = new File("C:\\Users\\Flavio\\Desktop", "StripMax.png");
+            new FindPixels().findByRangeColor(imageFile, 1044 + 0, 912, 42, 46, new Triplet<>(118,167,129), new Triplet<>(146,174,161));
+            //System.out.println("Area cannon 1: " + area);
             
-            area = new FindPixels().pixelContainsColorByRange(1095, 910, 42, 40, new Triplet<>(90,77,52), new Triplet<>(90,77,52));
-            System.out.println("Area cannon 2: " + area);
+            new FindPixels().findByRangeColor(imageFile, 1044 + 51, 912, 42, 46, new Triplet<>(118,167,129), new Triplet<>(146,174,161));
+            //System.out.println("Area cannon 2: " + area);
             
-            System.out.println((System.currentTimeMillis() - start)/100 + " secs");
+            System.out.println((System.currentTimeMillis() - start)/1000 + " secs");
         } catch (IOException ex) {
             System.out.println("Error: " + ex);
         }
