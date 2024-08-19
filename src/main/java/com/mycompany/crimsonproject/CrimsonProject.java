1e4567cc781ed1ffc35b9bf480c652838850b169
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-08-19 09:48:50 -0300

    Refatoração em CrimsonProject.java

--- src/main/java/com/mycompany/crimsonproject/CrimsonProject.java
+++ src/main/java/com/mycompany/crimsonproject/CrimsonProject.java
@@ -3,7 +3,6 @@ package com.mycompany.crimsonproject;
 import com.mycompany.crimsonproject.findpixels.FindPixels;
 import java.io.File;
 import java.io.IOException;
-import org.javatuples.Triplet;
 
 /**
  *
@@ -14,14 +13,14 @@ public class CrimsonProject {
     public static void main(String[] args) {
         try {
             double start = System.currentTimeMillis();
-            File imageFile = new File("C:\\Users\\Flavio\\Desktop", "StripMax.png");
-            new FindPixels().findByRangeColor(imageFile, 1044 + 0, 912, 42, 46, new Triplet<>(118,167,129), new Triplet<>(146,174,161));
+            File imageFile = new File("C:\\Users\\Flavio\\Desktop", "e11.png");
+            new FindPixels().findByGreenColor(imageFile, 1044 + 0, 912, 42, 46, 100, 125, 100);
             //System.out.println("Area cannon 1: " + area);
-            
-            new FindPixels().findByRangeColor(imageFile, 1044 + 51, 912, 42, 46, new Triplet<>(118,167,129), new Triplet<>(146,174,161));
+
+            new FindPixels().findByGreenColor(imageFile, 1044 + 51, 912, 42, 46, 100, 125, 100);
             //System.out.println("Area cannon 2: " + area);
-            
-            System.out.println((System.currentTimeMillis() - start)/1000 + " secs");
+
+            System.out.println((System.currentTimeMillis() - start) / 1000 + " secs");
         } catch (IOException ex) {
             System.out.println("Error: " + ex);
         }
