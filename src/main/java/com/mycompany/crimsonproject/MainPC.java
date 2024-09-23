4b594666b9927f7126e0cb27210746ac0bd2f3e7
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-09-23 00:14:13 -0300

    Alteração em MainPC class
    - Alterando chamada para se adaptar com o algoritmo

--- src/main/java/com/mycompany/crimsonproject/MainPC.java
+++ src/main/java/com/mycompany/crimsonproject/MainPC.java
@@ -8,9 +8,11 @@ import com.mycompany.crimsonproject.scripts.SetDestination;
 import com.mycompany.crimsonproject.soundlogs.SoundAlert;
 import java.awt.AWTException;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import net.sourceforge.tess4j.TesseractException;
+import org.javatuples.Pair;
 
 /**
  *
@@ -19,9 +21,11 @@ import net.sourceforge.tess4j.TesseractException;
 public class MainPC {
 
     public static void main(String[] args) throws InterruptedException {
+        String path = System.getProperty("user.dir") + "\\src\\main\\java\\com\\mycompany\\crimsonproject\\IOlogs\\logsfiles\\switchbelt.txt";
         final int GOTO_MININGBOT = 1;
         boolean isSwitchable = true;
         int waitForWarp = 55000;
+
         Thread.sleep(4000);
 
         for (int i = 0; i < 27; i++) {
@@ -31,18 +35,19 @@ public class MainPC {
                 new CargoDeposit().startScript();
                 Thread.sleep(12000);
 
-                new SetDestination(new R1920x1080().getAstBeltIList(), GOTO_MININGBOT, waitForWarp).startScript();
+                List<Pair<Integer, Integer>> astBelt = new R1920x1080().getAstBeltsMap().get(new TextLogs().readLine(path));
+                new SetDestination(astBelt, GOTO_MININGBOT, waitForWarp).startScript();
 
                 boolean isFalse = new ExtractOre(isSwitchable, waitForWarp).startScript();
                 if (!isFalse) {
                     break;
                 }
 
+                new TextLogs().timePerRoute("C:\\Users\\Flavio\\Desktop\\spr.txt", Long.toString((System.currentTimeMillis() - start) / 1000) + "\n");
+
             } catch (InterruptedException | IOException | AWTException | TesseractException ex) {
                 Logger.getLogger(MainPC.class.getName()).log(Level.SEVERE, null, ex);
             }
-
-            new TextLogs().timePerRoute("C:\\Users\\Flavio\\Desktop\\spr.txt", Long.toString((System.currentTimeMillis() - start) / 1000) + "\n");
         }
 
         //end of mining
