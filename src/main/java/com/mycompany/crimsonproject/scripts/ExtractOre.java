7c6a787d59af15d5fbcc1e0d6144db33f9ea6abf
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-09-24 20:25:26 -0300

    Alteração em ExtractOre class
    - Eliminando método não utilizado activeCannons

--- src/main/java/com/mycompany/crimsonproject/scripts/ExtractOre.java
+++ src/main/java/com/mycompany/crimsonproject/scripts/ExtractOre.java
@@ -149,13 +149,11 @@ public class ExtractOre implements VerifyRectangle {
                         this.walkThrough = 0; // reset script
                     }
                 }
-
             }
 
             case 2 -> {
                 this.timeStartSetAnotherAst = System.currentTimeMillis();
                 this.launchDrones();
-                //this.activeCannons();
                 this.engageDrones(); // engage drones
                 this.walkThrough++; // go to case 3
             }
@@ -264,30 +262,6 @@ public class ExtractOre implements VerifyRectangle {
         }
         return false;
 
-    }
-
-    private void activeCannons() throws IOException, InterruptedException, AWTException {
-
-        List<Integer> cannons = Arrays.asList(KeyEvent.VK_F1, KeyEvent.VK_F2);
-
-        for (int i = 0; i < cannons.size(); i++) {
-
-            if (this.isCannonActivated(i, 9,
-                    (Arrays.asList(R1920x1080.getF1CANNON1_X(), R1920x1080.getF2CANNON2_X())), R1920x1080.getFNCANNON_Y(),
-                    R1920x1080.getCANNON_H1(), R1920x1080.getCANNON_W1(),
-                    100, 115, 100)) {
-
-                new KeyboardEvents().clickKey(cannons.get(i));
-                Thread.sleep(CANNON_SLEEP);
-                new KeyboardEvents().clickKey(cannons.get(i));
-                System.out.println("The cannon was active. Press 2x cannon " + i);
-
-            } else {
-                Thread.sleep(CANNON_SLEEP); // Wait if cannon was canceled
-                new KeyboardEvents().clickKey(cannons.get(i));
-                System.out.println("Just press 1x cannon " + i);
-            }
-        }
     }
 
     private int checkCannonsAction() throws InterruptedException, AWTException, IOException {
