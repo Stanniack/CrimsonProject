11200a2cd9a8be21843d0f5b774038a67a2c1d65
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-10-26 02:27:45 -0300

    CalendarUtils: Alterando valor de ssHour
    - De 7 para 11 a.m horário de Reykjavik

--- src/main/java/com/mycompany/crimsonproject/utils/CalendarUtils.java
+++ src/main/java/com/mycompany/crimsonproject/utils/CalendarUtils.java
@@ -16,7 +16,7 @@ public class CalendarUtils {
      * @return false if it is possible continue mining or true if it is server save time
      */
     public boolean isServerSave(int minutes) {
-        int ssHour = 7, fullHour = 60;
+        int ssHour = 11, fullHour = 60;
         ZoneId zoneId = ZoneId.of("Atlantic/Reykjavik");
         ZonedDateTime nowInReykjavik = ZonedDateTime.now(zoneId);
         LocalTime localTime = nowInReykjavik.toLocalTime();
