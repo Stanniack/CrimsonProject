721cdbbc6765cb7ad58388e222589dfd4eb20c59
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-10-27 18:53:05 -0300

    Adicionando classe EndOfScriptException
    - Adicionando exceção EndOfScriptException

---
+++ src/main/java/com/mycompany/crimsonproject/exceptions/EndOfScriptException.java
@@ -0,0 +1,12 @@
+package com.mycompany.crimsonproject.exceptions;
+
+/**
+ *
+ * @author Devmachine
+ */
+public class EndOfScriptException extends Exception {
+
+    public EndOfScriptException(String message) {
+        super(message);
+    }
+}
