d36c33e66c42c26b765677c299ed74790eb9c7e5
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-11-22 08:21:37 -0300

    Update MainExtractOre.java

--- src/main/java/com/mycompany/crimsonproject/MainExtractOre.java
+++ src/main/java/com/mycompany/crimsonproject/MainExtractOre.java
@@ -1,6 +1,8 @@
 package com.mycompany.crimsonproject;
 
+import com.mycompany.crimsonproject.resolutions.R1920x1080;
 import com.mycompany.crimsonproject.scripts.ExtractOre;
+import com.mycompany.crimsonproject.scripts.SetDestination;
 import java.awt.AWTException;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -16,12 +18,17 @@ public class MainExtractOre {
 
     public static void main(String[] args) {
         boolean isSwitchable = true;
-        int giveAtry = 9;
+        int attempts = 9;
+        int returnDroneMS = 10000;
+        final int GOTO_HOMESTATION = 0;
+        int waitForWarp = 35000;
+        boolean isCheckWarpable = true;
         Triplet<Integer, Integer, Integer> tonsOfGreen = new Triplet(100, 125, 100);
 
         try {
             Thread.sleep(4000);
-            new ExtractOre(isSwitchable, giveAtry, tonsOfGreen)
+            new SetDestination(new R1920x1080().getHomeStationList(), GOTO_HOMESTATION, waitForWarp, isCheckWarpable, new Triplet<>(192, 192, 192)).startScript();
+            new ExtractOre(isSwitchable, attempts, tonsOfGreen, returnDroneMS)
                     .startScript();
         } catch (InterruptedException | IOException | AWTException | TesseractException ex) {
             Logger.getLogger(CrimsonProject.class.getName()).log(Level.SEVERE, null, ex);
