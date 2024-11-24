dc74bed27f57ca7558fcb0aed3ae053077154415
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-11-24 11:11:57 -0300

    Arquivo .json primário
    - Este arquivo será utilizado para guardar as informações de um retângulo

---
+++ src/main/java/com/mycompany/crimsonproject/scripts/CargoDepositS.java
@@ -0,0 +1,176 @@
+package com.mycompany.crimsonproject.scripts;
+
+import com.mycompany.crimsonproject.IOlogs.JsonLogs;
+import com.mycompany.crimsonproject.handlers.NetworkConnectionHandler;
+import com.mycompany.crimsonproject.findpixels.FindPixels;
+import com.mycompany.crimsonproject.robot.ClickScreenEvents;
+import com.mycompany.crimsonproject.robot.TakeScreenshot;
+import com.mycompany.crimsonproject.t4j.SegmentedRegions;
+import com.mycompany.crimsonproject.resolutions.R1920x1080Small;
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.io.IOException;
+
+import net.sourceforge.tess4j.TesseractException;
+import com.mycompany.crimsonproject.utils.RGBrange;
+import org.javatuples.Triplet;
+import com.mycompany.crimsonproject.interfaces.RectangleAndColorVerifier;
+import com.mycompany.crimsonproject.interfaces.RectangleVerifier;
+
+/**
+ *
+ * @author Devmachine
+ *
+ */
+public class CargoDepositS implements RectangleVerifier, RectangleAndColorVerifier {
+
+// Attributes related to graphical interface and screen manipulation
+    private final R1920x1080Small resolution;
+    private final RGBrange rgbr;
+    private final FindPixels findPixels;
+    private final ClickScreenEvents clickEvents;
+    private final SegmentedRegions segmentedRegions;
+    private final TakeScreenshot takeScreenshot;
+
+// Constants for mouse click actions
+    private static final int RIGHTCLICK = 0;
+    private static final int LEFTCLICK = 1;
+
+// Attributes related to specific UI elements
+    private Rectangle hangarButton;
+
+// switch-case behaviour attributes
+    private boolean isRunnable = true;
+    private boolean isStaticClicker = false;
+    private int walkThrough = 0;
+    private static final int STEPS = 2;
+    private NetworkConnectionHandler connectionHandler;
+    private JsonLogs jsonLogs;
+
+    public CargoDepositS() {
+        resolution = new R1920x1080Small();
+        rgbr = new RGBrange();
+
+        findPixels = new FindPixels();
+        clickEvents = new ClickScreenEvents();
+        segmentedRegions = new SegmentedRegions();
+        takeScreenshot = new TakeScreenshot();
+        connectionHandler = new NetworkConnectionHandler();
+        jsonLogs = new JsonLogs();
+    }
+
+    public boolean startScript() throws InterruptedException, IOException, AWTException, TesseractException {
+
+        while (walkThrough <= STEPS) {
+            // If there is net, continue script
+            if (connectionHandler.networkVerifier()) {
+                takeScreenshot.take();
+                flowScript();
+
+            } else {
+                isRunnable = false;
+                break;
+            }
+        }
+        return isRunnable;
+    }
+
+    private void flowScript() throws AWTException, InterruptedException, IOException, TesseractException {
+
+        switch (walkThrough) {
+
+            case 0 -> {
+                hangarButton = segmentedRegions.getRectangle(resolution.getHangarList(), resolution.getInventoryDeadzoneTuple());
+
+                if (rectangleAndColorVerifier(hangarButton, "HANGAR", 0, rgbr.getMinDestination(), rgbr.getMaxDestination())) {
+                    walkThrough++;
+                } else {
+                    clickEvents.dragScreen();
+                }
+            }
+
+            case 1 -> {
+                dragItens();
+                walkThrough++;
+                hangarButton = null;
+            }
+
+            case 2 -> {
+                if ((isStaticClicker && staticCheck()) || this.dynamicCheck()) {
+                    walkThrough++;
+                } else {
+                    clickEvents.dragScreen();
+                }
+            }
+
+        } // end switch
+    }
+
+    private void dragItens() throws AWTException, InterruptedException {
+        clickEvents.dragItemsToInventory(resolution.getDragItensDeadZoneList(), hangarButton);
+    }
+
+    @Override
+    public boolean rectangleVerifier(Rectangle rectangle, String itemName, int chosenClick) throws AWTException, InterruptedException {
+
+        /* For a millis seconds to take another screenshot, if not waiting by, the new screenshot doesn't take the right float window for click. */
+        if (rectangle != null) {
+            System.out.printf("Rect found (%s): Width: %d and Height: %d\n\n", itemName, rectangle.width, rectangle.height);
+
+            if (chosenClick == LEFTCLICK) {
+                clickEvents.leftClickCenterButton(rectangle);
+            } else {
+                clickEvents.rightClickCenterButton(rectangle);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean rectangleAndColorVerifier(Rectangle rect, String itemName, int chosenClick, Triplet<Integer, Integer, Integer> minRGB, Triplet<Integer, Integer, Integer> maxRGB) throws AWTException, InterruptedException, IOException {
+
+        /* For a millis seconds to take another screenshot, if not waiting by, the new screenshot doesn't take the right float window for click. */
+        if (rect != null && findPixels.findByRangeColor(rect.x, rect.y, rect.width, rect.height, minRGB, maxRGB)) {
+            System.out.printf("Rect found (%s): Width: %d and Height: %d - (%d, %d)\n\n", itemName, rect.width, rect.height, rect.x, rect.y);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isClickerDeprecated(String clicker) {
+        return (boolean) jsonLogs.readClicker(clicker).get("deprecated");
+    }
+
+    private boolean saveClicker(Rectangle rect, String clicker) {
+        return jsonLogs.saveClicker(rect, clicker);
+    }
+
+    private Rectangle getClicker(String clicker) {
+        return jsonLogs.getClicker(clicker);
+    }
+
+    private boolean staticCheck() throws IOException, TesseractException, AWTException, InterruptedException {
+        // If true, search a clicker and save it
+        if (isClickerDeprecated("undock")) {
+            Rectangle undockButton = segmentedRegions.getRectangle(resolution.getUndockButtonList(), resolution.getUndockDeadZoneTuple());
+
+            // if true save clicker to another cycle, else there is a clicker saved
+            if (rectangleVerifier(undockButton, "DYNAMICALLY IDENTIFIED UNDOCK BUTTON", LEFTCLICK)) {
+                saveClicker(undockButton, "undock");
+                return true;
+            }
+
+        } else {
+            rectangleVerifier(this.getClicker("undock"), "STATIC CLICKER UNDOCK BUTTON", LEFTCLICK);
+            return true;
+        }
+        return false;
+    }
+
+    private boolean dynamicCheck() throws IOException, TesseractException, AWTException, InterruptedException {
+        Rectangle undockButton = segmentedRegions.getRectangle(resolution.getUndockButtonList(), resolution.getUndockDeadZoneTuple());
+        return rectangleVerifier(undockButton, "DYNAMICALLY IDENTIFIED UNDOCK BUTTON", LEFTCLICK);
+    }
+
+}
