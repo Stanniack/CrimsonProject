bbbdc5295f4fa94e2d7a2102b5085f653946abc5
Mateus Vitor <44790156+Stanniack@users.noreply.github.com>
2024-12-07 22:48:40 -0300

    WindowsServiceHandler: Classe handler para lidar com comportamento de janelas

---
+++ src/main/java/com/mycompany/crimsonproject/handlers/WindowsServiceHandler.java
@@ -0,0 +1,41 @@
+package com.mycompany.crimsonproject.handlers;
+
+import com.mycompany.crimsonproject.windowschecker.WindowsService;
+import org.javatuples.Triplet;
+
+/**
+ *
+ * @author Devmachine
+ */
+public class WindowsServiceHandler {
+
+    private String GUIname = "EVE";
+    private String exeName = "exefile.exe";
+    private WindowsService wService;
+    private SleeperHandler sleeper;
+
+    public WindowsServiceHandler() {
+        this.wService = wService = new WindowsService();
+        this.sleeper = sleeper = new SleeperHandler();
+    }
+
+    /**
+     * Verifies the status of a window based on its name, executable, and
+     * fullscreen state. If the conditions are not met, it will continuously
+     * check and wait for the conditions to be true.
+     *
+     * @param ms The time (in milliseconds) to wait between checks when the
+     * conditions are not met.
+     */
+    public void windowsVerifier(Integer ms) {
+        Triplet<String, String, Boolean> GUIstats = wService.activeWindowsChecker();
+        boolean constainsGUIname = GUIstats.getValue0().contains(GUIname);
+        boolean equalsExe = GUIstats.getValue1().equals(exeName);
+        boolean isFullscreen = GUIstats.getValue2();
+        System.out.println(!(constainsGUIname && equalsExe && isFullscreen) ? "The script was interupted. Check your EVE.exe" : null);
+
+        while (!(constainsGUIname && equalsExe && isFullscreen)) {
+            sleeper.sleep(ms);
+        }
+    }
+}
